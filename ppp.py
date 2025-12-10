09c60896936e4d8969d3f8e658dc97ebdba060ec
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-12-10 15:31:18 -0500

    Update ppp.py

--- ppp.py
+++ ppp.py
@@ -2,11 +2,12 @@ import time
 
 class pest():  
 
-    def __init__(self, name, happiness, hunger, exhaustion):
+    def __init__(self, name, happiness, hunger, exhaustion, level):
         self.name = name 
         self.__happiness = happiness 
         self.hunger = hunger
         self.exhaustion = exhaustion
+        self.level = level
 
     def show_status(self):
         print(f"{self.name}'s happiness is now {self.__happiness}.")
@@ -25,75 +26,87 @@ class pest():
             if  playtime == "play":
                 print(f"{self.name} is playing fetch!")
                 self.__happiness += 10
-                self.hunger -= 10
+                self.hunger -= 100
                 self.exhaustion += 10
                 self.show_status()
                 self.show_status2()
                 if self.hunger <= 0:
-                    time.sleep(3)
-                    print("...")
-                    time.sleep(3)
-                    print(f"{self.name} has died.")
-                    time.sleep(3)
-                    print(f"You did not feed {self.name} enough.")
-                    time.sleep(3)
-                    print("Do you know what you've done?")
-                    time.sleep(3)
-                    print(f"You've starved {self.name}.")
-                    time.sleep(3)
-                    print(f"{self.name}'s dead.")
-                    time.sleep(3)
-                    print("You're a monster.")
-                    time.sleep(3)
-                    print("An idiotic monster.")
-                    time.sleep(3)
-                    print("You can't tell me that you didn't know,")
-                    time.sleep(3)
-                    print("that your pet could die from starvation.")
-                    time.sleep(3)
-                    print("Are you allergic to common sense?")
-                    time.sleep(3)
-                    print("Look at what you are playing right now.")
-                    time.sleep(3)
-                    print("A pet simulator game.")
-                    time.sleep(3)
-                    print("Why would you ever play this game in the first place?")
-                    time.sleep(3)
-                    print(f"So you can neglect {self.name}?")
-                    time.sleep(3)
-                    print(f"So you can abuse {self.name}?")
-                    time.sleep(3)
-                    print(f"So you can starve {self.name}?")
-                    time.sleep(3)
-                    print(f"If you can so willingly kill {self.name},")
-                    time.sleep(3)
-                    print("tell me why you can't come back and kill more pets.")
-                    time.sleep(3)
-                    print(f"{self.name} will never come back.")
-                    time.sleep(3)
-                    print("You know that, don't you?")
-                    time.sleep(3)
-                    print(f"{self.name}'s corpse lies at your feet.")
-                    time.sleep(3)
-                    print("Look at its thin, raggedy body.")
-                    time.sleep(3)
-                    print("Go ahead.")
-                    time.sleep(3)
-                    print("Take a look.")
-                    time.sleep(3)
-                    print("Do you not feel even a bit of remorse?")
-                    time.sleep(3)
-                    print("Regret?")
-                    time.sleep(3)
-                    print("Guilt?")
-                    time.sleep(3)
-                    print("I thought not.")
-                    time.sleep(3)
-                    print("Remember the horror that you've done today.")
-                    time.sleep(3)
-                    print("I won't forget this.")
-                    time.sleep(3)
-                    print("Goodbye.")
+                    messages = [
+                        "...",
+                        f"{self.name} has died.",
+                        f"You did not feed {self.name} enough.",
+                        "Do you know what you've done?",
+                        f"You've starved {self.name}.",
+                        f"{self.name}'s dead.",
+                        "You're a monster.",
+                        "An idiotic monster.",
+                        "You can't tell me that you didn't know,",
+                        "that your pet could die from starvation.",
+                        "Are you allergic to common sense?",
+                        "Look at what you are playing right now.",
+                        "A pet simulator game.",
+                        "Why would you ever play this game in the first place?",
+                        f"So you can neglect {self.name}?",
+                        f"So you can abuse {self.name}?",
+                        f"So you can starve {self.name}?",
+                        f"If you can so willingly kill {self.name},",
+                        "tell me why you can't come back and kill more pets.",
+                        f"{self.name} will never come back.",
+                        "You know that, don't you?",
+                        f"{self.name}'s corpse lies at your feet.",
+                        "Look at its thin, raggedy body.",
+                        "Go ahead.",
+                        "Take a look.",
+                        "Do you not feel even a bit of remorse?",
+                        "Regret?",
+                        "Guilt?",
+                        "I thought not.",
+                        "Remember the horror that you've done today.",
+                        "I won't forget this.",
+                        "Goodbye."
+                    ]
+                    for line in messages:
+                        time.sleep(3)
+                        print(line)
+                    break
+                if self.exhaustion <= 0:
+                    messages = [
+                        "...",
+                        f"{self.name} has died.",
+                        f"You did not allow {self.name} to rest enough.",
+                        "Do you know what you've done?",
+                        f"You've overworked {self.name}.",
+                        f"{self.name}'s dead.",
+                        "You're a monster.",
+                        "An idiotic monster.",
+                        "You can't tell me that you didn't know,",
+                        "that your pet could die from exhaustion.",
+                        "Are you allergic to common sense?",
+                        "Look at what you are playing right now.",
+                        "A pet simulator game.",
+                        "Why would you ever play this game in the first place?",
+                        f"So you can overwork {self.name}?",
+                        "So you can force playtime for your own happiness?",
+                        f"So you can make {self.name} do whatever you want?",
+                        f"If you can so willingly kill {self.name},",
+                        "tell me why you can't come back and kill more pets.",
+                        f"{self.name} will never come back.",
+                        "You know that, don't you?",
+                        f"{self.name}'s corpse lies at your feet.",
+                        "Look at its limp, broken body.",
+                        "Go ahead.",
+                        "Take a look.",
+                        "Do you not feel even a bit of remorse?",
+                        "Regret?",
+                        "Guilt?",
+                        "I thought not.",
+                        "Remember the horror that you've done today.",
+                        "I won't forget this.",
+                        "Goodbye."
+                    ]
+                    for line in messages:
+                        time.sleep(3)
+                        print(line)
                     break
 
             elif playtime == "feed":
@@ -102,70 +115,43 @@ class pest():
                 self.show_status2()
                 print(f"{self.name}'s happiness is still {self.__happiness}. ")
                 if self.hunger >= 100:
-                    time.sleep(3)
-                    print("...")
-                    time.sleep(3)
-                    print(f"{self.name} has died.")
-                    time.sleep(3)
-                    print(f"You fed {self.name} too much.")
-                    time.sleep(3)
-                    print("Do you know what you've done?")
-                    time.sleep(3)
-                    print(f"You've overfed {self.name}.")
-                    time.sleep(3)
-                    print(f"{self.name}'s dead.")
-                    time.sleep(3)
-                    print("You're a monster.")
-                    time.sleep(3)
-                    print("An idiotic monster.")
-                    time.sleep(3)
-                    print("You can't tell me that you didn't know,")
-                    time.sleep(3)
-                    print("that your pet could die from overeating.")
-                    time.sleep(3)
-                    print("Are you allergic to common sense?")
-                    time.sleep(3)
-                    print("Look at what you are playing right now.")
-                    time.sleep(3)
-                    print("A pet simulator game.")
-                    time.sleep(3)
-                    print("Why would you ever play this game in the first place?")
-                    time.sleep(3)
-                    print(f"So you can stuff {self.name}?")
-                    time.sleep(3)
-                    print(f"So you can overfeed {self.name}?")
-                    time.sleep(3)
-                    print("So you can can go without trouble in your life?")
-                    time.sleep(3)
-                    print(f"If you can so willingly kill {self.name},")
-                    time.sleep(3)
-                    print("tell me why you can't come back and kill more pets.")
-                    time.sleep(3)
-                    print(f"{self.name} will never come back.")
-                    time.sleep(3)
-                    print("You know that, don't you?")
-                    time.sleep(3)
-                    print(f"{self.name}'s corpse lies at your feet.")
-                    time.sleep(3)
-                    print("Look at its stuffed, bloated body.")
-                    time.sleep(3)
-                    print("Go ahead.")
-                    time.sleep(3)
-                    print("Take a look.")
-                    time.sleep(3)
-                    print("Do you not feel even a bit of remorse?")
-                    time.sleep(3)
-                    print("Regret?")
-                    time.sleep(3)
-                    print("Guilt?")
-                    time.sleep(3)
-                    print("I thought not.")
-                    time.sleep(3)
-                    print("Remember the horror that you've done today.")
-                    time.sleep(3)
-                    print("I won't forget this.")
-                    time.sleep(3)
-                    print("Goodbye.")
+                    messages = [
+                        "...",
+                        f"{self.name} has died.",
+                        f"You fed {self.name} too much.",
+                        "Do you know what you've done?",
+                        f"You've overfed {self.name}.",
+                        f"{self.name}'s dead.",
+                        "You're a monster.",
+                        "An idiotic monster.",
+                        "You can't tell me that you didn't know,",
+                        "that your pet could die from overeating.",
+                        "Are you allergic to common sense?",
+                        "Look at what you are playing right now.",
+                        "A pet simulator game.",
+                        "Why would you ever play this game in the first place?",
+                        f"So you can stuff {self.name}?",
+                        f"So you can overlook {self.name}?",
+                        "So you can go without trouble in your life?",
+                        f"If you can so willingly kill {self.name},",
+                        "tell me why you can't come back and kill more pets.",
+                        f"{self.name} will never come back.",
+                        "You know that, don't you?",
+                        f"{self.name}'s corpse lies at your feet.",
+                        "Look at its stuffed, bloated body.",
+                        "Go ahead.",
+                        "Take a look.",
+                        "Do you not feel even a bit of remorse?",
+                        "Regret?",
+                        "Guilt?",
+                        "I thought not.",
+                        "Remember the horror that you've done today.",
+                        "I won't forget this.",
+                        "Goodbye."
+                    ]
+                    for line in messages:
+                        time.sleep(3)
+                        print(line)
                     break
 
             elif playtime == "sleep":
@@ -181,5 +167,8 @@ class pest():
                 print(f"{self.name}'s hunger level is still {self.hunger}. ")  
 
 pestname = input("What is your pet's name? ")
-pest = pest(pestname, 50, 100, 0)
+#intro text here
+pest = pest(pestname, 50, 50, 0, 1)
 pest.play()
+
+#add evo, chests, levels, cookie gambling, limit amount of actions
